934dc4cd5d41a3b8f14a0b5b1257eecf03d9f324
ed-commits <edward.alexander.cant@gmail.com>
2020-11-24 19:56:16 +0000

    add a simple login form with basic templates

--- whatsup/whatsupapp/urls.py
+++ whatsup/whatsupapp/urls.py
@@ -1,7 +1,11 @@
+from django.conf.urls import url
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
 ]
