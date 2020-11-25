ca2a1af6fa9ba5220e0ac6c7d4315e1a17ada1b2
ed-commits <edward.alexander.cant@gmail.com>
2020-11-25 16:51:13 +0000

    urls.py: use accounts/ for the log in and log out pages

--- whatsup/whatsupapp/urls.py
+++ whatsup/whatsupapp/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
-    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
-    url(r'^register/$', views.register, name='register'),
-    url(r'^profile/$', views.profile, name='profile'),
+#    url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
+#    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+#    url(r'^register/$', views.register, name='register'),
+    url('profile', views.profile, name='profile'),
 ]
