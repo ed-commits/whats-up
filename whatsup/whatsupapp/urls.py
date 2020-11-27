7c40ec01bef0fa0da6e07d7d4a5f2740a6e7a76f
ed-commits <edward.alexander.cant@gmail.com>
2020-11-27 11:39:24 +0000

    Create some test users (the beatles) and create a page /app/everyone that lists all users

--- whatsup/whatsupapp/urls.py
+++ whatsup/whatsupapp/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
 #    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
 #    url(r'^register/$', views.register, name='register'),
     url('profile', views.profile, name='profile'),
+    url('everyone', views.everyone, name='everyone'),
 ]
--- whatsup/whatsupapp/views.py
+++ whatsup/whatsupapp/views.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import get_user_model
 
 def index(request):
     return render(request, 'index.html', {})
@@ -12,3 +13,9 @@ def profile(request):
     if not request.user.is_authenticated:
         return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
     return render(request, 'profile.html', {'user': request.user})
+
+def everyone(request):
+    # https://stackoverflow.com/a/23139674
+    User = get_user_model()
+    users = User.objects.all()
+    return render(request, 'everyone.html', {'users_list': users})
