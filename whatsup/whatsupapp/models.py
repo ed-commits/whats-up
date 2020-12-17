cbee033b156ba464088cc51274d236024ac6782e
Edward Cant <edward.alexander.cant@gmail.com>
2020-12-17 13:19:54 +0000

    Display date of birth, and computed property age on the profile page.

--- whatsup/whatsupapp/models.py
+++ whatsup/whatsupapp/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from datetime import date
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     dateofbirth = models.DateField()
+
+    @property
+    def age(self):
+        today = date.today()
+        return today.year - self.dateofbirth.year - ((today.month, today.day) < (self.dateofbirth.month, self.dateofbirth.day))
